ba0382e154c47cb1ccae518c5a4cc328579a2357
Junaid Qureshi <Junaid.562000@gmail.com>
2025-11-08 17:39:21 +0530

    .

--- middleware.ts
+++
@@ -1,9 +0,0 @@
-// middleware.ts
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-
-export function middleware(request: NextRequest) {
-  const response = NextResponse.next();
-  response.headers.set("x-pathname", request.nextUrl.pathname);
-  return response;
-}
--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  images: {
-    domains: ["www.mujtamaglobal.com", "www.images.pexels.com"],
-  },
-};
-
-export default nextConfig;
